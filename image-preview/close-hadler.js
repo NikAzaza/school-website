1e3f51897f00431ad394210c8f99a030aa35147e
NikAzaza <30291419+NikAzaza@users.noreply.github.com>
2020-07-12 19:12:23 +0300

    close by click outside img click

--- image-preview/close-hadler.js
+++ image-preview/close-hadler.js
@@ -1,3 +1,4 @@
-function closeImage() {
-  document.getElementById('images-full-size-container').classList.toggle('opened');
+function closeHandler(container, event){
+    if (!event.path.includes(container.getElementsByTagName('img')[0]))
+    document.getElementById('images-full-size-container').classList.toggle('opened');
 }
