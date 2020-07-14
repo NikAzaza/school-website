8c71594eeb6165e7b86287da7e844df424ced590
NikAzaza <30291419+NikAzaza@users.noreply.github.com>
2020-07-14 21:17:07 +0300

    fixed opening after click outside 2.0.0
    fixed opening modal window with an image inside after click outside body 2.0.0

--- image-preview/close-hadler.js
+++ image-preview/close-hadler.js
@@ -1,4 +1,4 @@
 function closeHandler(container, event){
     if (!event.path.includes(container.getElementsByTagName('img')[0]))
-    document.getElementById('images-full-size-container').classList.toggle('opened');
+    document.getElementById('images-full-size-container').classList.remove('opened');
 }
